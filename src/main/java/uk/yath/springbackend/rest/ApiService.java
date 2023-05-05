8704fbfb682d3253daf2f61b954cd7660c5a241d
yatharthasharma <yatharthasharma09@gmail.com>
2023-05-05 01:31:18 +0100

    Add Kafka setup to be able to publish messages

--- src/main/java/uk/yath/springbackend/rest/ApiService.java
+++ src/main/java/uk/yath/springbackend/rest/ApiService.java
@@ -2,13 +2,22 @@ package uk.yath.springbackend.rest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ApiService {
     private static final Logger LOGGER = LoggerFactory.getLogger(ApiService.class);
 
+    public ApiService(@Autowired KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
     void operation() {
         LOGGER.info("Service operated on the GET request");
+        kafkaTemplate.send("test-topic", "Hello world!");
     }
 }
